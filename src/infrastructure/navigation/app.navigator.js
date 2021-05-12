3cd9dedcde069d360d8b922c48f568f74d13307b
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-05-12 14:15:38 +1000

    map set up

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Text } from 'react-native';
+import { SafeArea } from '../../components/utility/safe-area.component';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
 import { RestaurantsNavigator } from './restaurants.navigator';
+import { MapScreen } from '../../features/map/screens/map.screen';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,11 +19,6 @@ const TAB_ICON = {
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>
-);
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
   </SafeArea>
 );
 
@@ -45,7 +43,7 @@ export const AppNavigator = () => {
         }}
       >
         <Tab.Screen name='Restaurant' component={RestaurantsNavigator} />
-        <Tab.Screen name='Map' component={Map} />
+        <Tab.Screen name='Map' component={MapScreen} />
         <Tab.Screen name='Settings' component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
